7deae7d076351a5dcee0799ce0362ecacffdf205
Mike Rotondo <mrotondo@gmail.com>
2025-12-19 14:23:27 -0800

    switch ButtonLinkComponent to use button_to and take a method param that defaults to get (#11)
    * switch ButtonLinkComponent to use button_to and take a method param that defaults to get
    
    * fix case statement alignment
    
    * add turbo param to turn turbo off when needed
    
    * Revert "add turbo param to turn turbo off when needed"
    
    This reverts commit 2e76ed4c5b2b9c776768660facb25cde1ebd36c3.

--- app/components/button_link_component.rb
+++ app/components/button_link_component.rb
@@ -1,16 +1,20 @@
 # frozen_string_literal: true
 
 class ButtonLinkComponent < ViewComponent::Base
-  def initialize(label:, url:, style: :primary)
+  def initialize(label:, url:, style: :primary, method: :get)
     @label = label
     @url = url
-    @button_style = case style
-    when :primary
-                      "btn--primary"
-    when :secondary
-                      "btn--secondary"
-    else
-                      raise ArgumentError("Invalid button style")
-    end
+    @button_style =
+      case style
+      when :primary
+        "btn--primary"
+      when :secondary
+        "btn--secondary"
+      when :destructive
+        "btn--destructive"
+      else
+        raise ArgumentError("Invalid button style")
+      end
+    @method = method
   end
 end
