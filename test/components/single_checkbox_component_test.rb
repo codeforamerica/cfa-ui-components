6ac650976602eea7102f36915a606e2c8d0a15a5
Drew Proebstel <dproebstel@codeforamerica.org>
2025-12-24 09:39:36 -0800

    rubocop

--- test/components/single_checkbox_component_test.rb
+++ test/components/single_checkbox_component_test.rb
@@ -4,6 +4,6 @@ require "test_helper"
 
 class SingleCheckboxComponentTest < ViewComponent::TestCase
   def test_component_renders_something_useful
-    #tests will go here
+    # tests will go here
   end
 end
