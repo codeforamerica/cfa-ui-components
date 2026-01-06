1ef345828c1c6ab4fc20246375e5c27bef38bf7e
Drew Proebstel <d.proebstel@gmail.com>
2026-01-06 12:14:13 -0800

    single checkbox component (#17)
    * single checkbox component
    
    ---------
    
    Co-authored-by: Drew Proebstel <dproebstel@codeforamerica.org>

---
+++ test/components/single_checkbox_component_test.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class SingleCheckboxComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # tests will go here
+  end
+end
