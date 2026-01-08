048950b79d7e3f57b38058824f8888d3b7d66d0d
Kathryn Hansen <katie112892@gmail.com>
2026-01-08 13:59:29 -0600

    add expandable section component (#18)
    * add expandable section component
    
    * use purely tailwind css, add a content block for details, add a new base component with icon strategy
    
    * remove empty line
    
    * fix linting
    
    * fix css alignment
    
    * remove extra styling from the preview
    
    * show that you can adding margins
    
    * add new component with padding
    
    ---------
    
    Co-authored-by: Kathryn Hansen <kathrynhansen@Mac.lan>

---
+++ app/components/expandable_section_component.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class ExpandableSectionComponent < BaseComponent
+  def initialize(summary_text:, icon: nil)
+    @summary_text = summary_text
+    @icon = icon
+  end
+end
