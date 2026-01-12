7314333c18e9c47428426fccff3c1c37ec2fd670
Mike Rotondo <mrotondo@codeforamerica.org>
2026-01-12 15:47:37 -0800

    add support for value types to text fields and implement the TIN type by adding an appropriate input mask

--- app/components/text_field_component.rb
+++ app/components/text_field_component.rb
@@ -1,15 +1,27 @@
 # frozen_string_literal: true
 
 class TextFieldComponent < ViewComponent::Base
-  def initialize(form:, method:, label:, help_text: nil, required: false)
+  TIN = :tin
+
+  def initialize(form:, method:, label:, help_text: nil, value_type: nil, required: false)
     @form = form
     @method = method
     @label = label
     @help_text = help_text
+    @value_type = value_type
     @required = required
   end
 
   private
+
+  def text_field_kwargs
+    kwargs = {}
+    if @value_type == TIN
+      kwargs["x-mask"] = "999-99-9999"
+      kwargs["placeholder"] = "___-__-____"
+    end
+    kwargs
+  end
 
   def required_class
     @required ? "required" : ""
