7f136ffd566601310eefb0514d57627dbc17265b
Kathryn Hansen <katie112892@gmail.com>
2026-01-22 13:56:58 -0600

    add small size to the button component (#25)
    Co-authored-by: Kathryn Hansen <kathrynhansen@Mac.lan>

--- test/components/previews/button_link_component_preview.rb
+++ test/components/previews/button_link_component_preview.rb
@@ -10,4 +10,15 @@ class ButtonLinkComponentPreview < ViewComponent::Preview
   def destructive
     render(ButtonLinkComponent.new(label: I18n.t("remove"), url: "/destroy", style: :destructive, method: :post))
   end
+  def primary_small
+    render(ButtonLinkComponent.new(label: I18n.t("continue"), url: "https://www.google.com", size: :small))
+  end
+
+  def secondary_small
+    render(ButtonLinkComponent.new(label: I18n.t("decline"), url: "/decline", style: :secondary, size: :small))
+  end
+
+  def destructive_small
+    render(ButtonLinkComponent.new(label: I18n.t("remove"), url: "/destroy", style: :destructive, method: :post, size: :small))
+  end
 end
