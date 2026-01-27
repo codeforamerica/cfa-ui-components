5cbec5d8078b74ede86bc4644bef553ddd388972
Kathryn Hansen <katie112892@gmail.com>
2026-01-27 13:54:54 -0600

    rename expandable section to reveal (#32)
    Co-authored-by: Kathryn Hansen <kathrynhansen@Mac.lan>

--- test/components/reveal_component_test.rb
+++ test/components/reveal_component_test.rb
@@ -2,11 +2,11 @@
 
 require "test_helper"
 
-class ExpandableSectionComponentTest < ViewComponent::TestCase
+class RevealComponentTest < ViewComponent::TestCase
   def test_component_renders_something_useful
     # assert_equal(
     #   %(<span>Hello, components!</span>),
-    #   render_inline(ExpandableSectionComponent.new(message: "Hello, components!")).css("span").to_html
+    #   render_inline(RevealComponent.new(message: "Hello, components!")).css("span").to_html
     # )
   end
 end
