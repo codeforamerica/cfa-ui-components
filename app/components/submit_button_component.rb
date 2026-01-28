a501fd64a1e8fe8b6ecfeacd75eb7217cdef51f3
Mike Rotondo <mrotondo@codeforamerica.org>
2026-01-27 17:56:52 -0800

    add optional style param supporting primary and secondary to SubmitButtonComponent

--- app/components/submit_button_component.rb
+++ app/components/submit_button_component.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
 class SubmitButtonComponent < ViewComponent::Base
-  def initialize(form:, label: I18n.t("continue"))
+  def initialize(form:, label: I18n.t("continue"), style: :primary)
     @form = form
     @label = label
+    @button_style =
+      case style
+      when :primary
+        "btn--primary"
+      when :secondary
+        "btn--secondary"
+      else
+        raise ArgumentError("Invalid button style")
+      end
   end
 end
