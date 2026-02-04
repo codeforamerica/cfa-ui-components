455a6771988f939421a560fbf95216d76d42e8a4
Kathryn Hansen <kathrynhansen@Mac.lan>
2026-02-04 14:12:06 -0600

    change preview names

--- test/components/previews/icon_component_preview.rb
+++ test/components/previews/icon_component_preview.rb
@@ -1,13 +1,17 @@
 class IconComponentPreview < ViewComponent::Preview
-  def with_info
+  def info
     render(IconComponent.new(icon: "info"))
   end
 
-  def with_circle_xmark
+  def question
+    render(IconComponent.new(icon: "question"))
+  end
+
+  def circle_xmark
     render(IconComponent.new(icon: "circle_xmark"))
   end
 
-  def with_circle_check
+  def circle_check
     render(IconComponent.new(icon: "circle_check"))
   end
 end
