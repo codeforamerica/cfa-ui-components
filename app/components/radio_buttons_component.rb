d80a712302b366214a8402c649ac2f22253703a1
Tahsina Islam <79296603+tahsinaislam@users.noreply.github.com>
2026-03-06 13:44:31 -0600

    TEF-127 Update radio buttons layout (#45)
    Co-authored-by: Kathryn Hansen <kathrynhansen@Mac.lan>

--- app/components/radio_buttons_component.rb
+++ app/components/radio_buttons_component.rb
@@ -1,10 +1,19 @@
 # frozen_string_literal: true
 
 class RadioButtonsComponent < AttributeBoundFormElementComponent
-  def initialize(form:, method:, collection:, item_value_method:, item_label_method:)
+  def initialize(form:, method:, collection:, item_value_method:, item_label_method:, layout: :vertical)
     super(form: form, method: method)
     @collection = collection
     @item_value_method = item_value_method
     @item_label_method = item_label_method
+    @layout =
+      case layout
+      when :horizontal
+        "flex items-start gap-4"
+      when :vertical
+        "space-y-4"
+      else
+        raise ArgumentError("Invalid layout")
+      end
   end
 end
