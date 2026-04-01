589a8ac08fac50ca097f8aa03a39329da538ec5a
Kathryn Hansen <kathrynhansen@Mac.lan>
2026-03-31 20:51:42 -0500

    small fixes

--- app/components/number_field_component.rb
+++ app/components/number_field_component.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 class NumberFieldComponent < AttributeBoundFormElementComponent
-  def initialize(form:, method:, label:, max_num:, min_num: 0, step_num: 0.01, help_text: nil, value_type: nil, required: false)
+  def initialize(form:, method:, label:, max_num:, min_num: 0, step_num: 0.01, help_text: nil, required: false)
     super(form: form, method: method)
     @label = label
     @help_text = help_text
-    @value_type = value_type
     @required = required
     @min_num = min_num
     @max_num = max_num
--- test/components/previews/number_field_component_preview.rb
+++ test/components/previews/number_field_component_preview.rb
@@ -2,6 +2,9 @@ class NumberFieldComponentPreview <  FormComponentPreview
   def default
     render(NumberFieldComponent.new(form: form, method: :my_number, label: I18n.t(:my_number), min_num: 0, max_num: 1000))
   end
+  def required
+    render(NumberFieldComponent.new(form: form, method: :my_number, label: I18n.t(:my_number), min_num: 0, max_num: 1000, required: true))
+  end
 
   def with_helper_text
     render(NumberFieldComponent.new(form: form, method: :my_number, label: I18n.t(:my_number), help_text: "pick a number", min_num: 0, max_num: 1000))
