a4d4aca733367c446a924cd6d4735f086af74631
Kathryn Hansen <kathrynhansen@Kathryns-MacBook-Pro.local>
2026-04-03 13:15:47 -0500

    revamp input currenct

--- app/components/input_currency_component.rb
+++ app/components/input_currency_component.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
 
 class InputCurrencyComponent < AttributeBoundFormElementComponent
-  def initialize(form:, method:, label:, max_num:, min_num: 0, step_num: 0.01, help_text: nil, required: false)
+  def initialize(form:, method:, label:, help_text: nil,  **number_field_kwargs)
     super(form: form, method: method)
     @label = label
     @help_text = help_text
-    @required = required
-    @min_num = min_num
-    @max_num = max_num
-    @step_num = step_num
+    @number_field_kwargs = number_field_kwargs
   end
 end
--- test/components/previews/input_currency_component_preview.rb
+++ test/components/previews/input_currency_component_preview.rb
@@ -1,19 +1,19 @@
 class InputCurrencyComponentPreview <  FormComponentPreview
   def default
-    render(InputCurrencyComponent.new(form: form, method: :my_number, label: I18n.t(:my_number), min_num: 0, max_num: 1000))
+    render(InputCurrencyComponent.new(form: form, method: :my_number, label: I18n.t(:my_number), min: 0))
   end
   def required
-    render(InputCurrencyComponent.new(form: form, method: :my_number, label: I18n.t(:my_number), min_num: 0, max_num: 1000, required: true))
+    render(InputCurrencyComponent.new(form: form, method: :my_number, label: I18n.t(:my_number), min: 0, required: true))
   end
 
   def with_helper_text
-    render(InputCurrencyComponent.new(form: form, method: :my_number, label: I18n.t(:my_number), help_text: "pick a number", min_num: 0, max_num: 1000))
+    render(InputCurrencyComponent.new(form: form, method: :my_number, label: I18n.t(:my_number), help_text: "pick a number", min: 0))
   end
 
   def with_error
     custom_model = TestModel.new
     custom_model.valid?
     puts "custom_model.valid? #{custom_model.valid?}"
-    render(InputCurrencyComponent.new(form: form(model: custom_model), method: :my_number, label: I18n.t(:my_number), max_num: 1000))
+    render(InputCurrencyComponent.new(form: form(model: custom_model), method: :my_number, label: I18n.t(:my_number)))
   end
 end
