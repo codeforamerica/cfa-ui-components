12f562992e5c405ef47b0f2ee88b494af1cdbdbc
Kathryn Hansen <kathrynhansen@Kathryns-MacBook-Pro.local>
2026-04-03 14:51:33 -0500

    revamp input currency

--- app/components/input_currency_component.rb
+++ app/components/input_currency_component.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class InputCurrencyComponent < AttributeBoundFormElementComponent
-  def initialize(form:, method:, label:, help_text: nil,  **number_field_kwargs)
+  def initialize(form:, method:, label:, help_text: nil, required: false, **number_field_kwargs)
     super(form: form, method: method)
     @label = label
     @help_text = help_text
+    @required = required
     @number_field_kwargs = number_field_kwargs
   end
 end
