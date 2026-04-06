629bd6cda321fdf13ecd678680f50dba161bd16f
David Lowenfels <dlowenfels@codeforamerica.org>
2026-04-06 16:46:54 -0700

    Add cross-component and label/input association tests
    - Adds system tests for Alpine conditional behaviors (radio, checkbox, dropdown, pre-filled states)
    
    - Adds label/input id association tests for all input components
    
    - Adds cross-cutting error assertion test
    
    - Tests Modal button slots
    
    - Fix undefined [] error in preview_controller

--- app/controllers/preview_controller.rb
+++ app/controllers/preview_controller.rb
@@ -2,7 +2,7 @@ class PreviewController < ViewComponentsController
   around_action :switch_locale
 
   def switch_locale(&action)
-    locale = params[:lookbook][:display][:lang] || I18n.default_locale
+    locale = params.dig(:lookbook, :display, :lang) || I18n.default_locale
     I18n.with_locale(locale, &action)
   end
 end
