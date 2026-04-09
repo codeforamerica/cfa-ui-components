a667dac0bc16b5c42d02553f4b43241277eace81
DrewProebstelCfa <dproebstel@codeforamerica.org>
2026-04-09 12:59:46 -0700

    add required class and tests

--- test/components/tin_component_test.rb
+++ test/components/tin_component_test.rb
@@ -3,10 +3,39 @@
 require "test_helper"
 
 class TinComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(TinComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  def test_renders_label_and_input
+    render_inline(TinComponent.new(form: build_form, method: :text_field, label: "Social Security Number"))
+    assert_selector "label", text: "Social Security Number"
+    assert_selector "input[type='text']"
+  end
+
+  def test_renders_help_text
+    render_inline(TinComponent.new(form: build_form, method: :text_field, label: "Social Security Number", help_text: "Enter your SSN"))
+    assert_selector ".help_text", text: "Enter your SSN"
+  end
+
+  def test_required_adds_class_to_label
+    render_inline(TinComponent.new(form: build_form, method: :text_field, label: "Social Security Number", required: true))
+    assert_selector "label.required"
+  end
+
+  def test_renders_show_toggle_checkbox
+    render_inline(TinComponent.new(form: build_form, method: :text_field, label: "Social Security Number"))
+    assert_selector "input[type='checkbox']"
+  end
+
+  def test_input_has_tin_mask
+    render_inline(TinComponent.new(form: build_form, method: :text_field, label: "Social Security Number"))
+    assert_selector "input[x-mask='999-99-9999']"
+  end
+
+  def test_input_has_tin_placeholder
+    render_inline(TinComponent.new(form: build_form, method: :text_field, label: "Social Security Number"))
+    assert_selector "input[placeholder='000-00-0000']"
+  end
+
+  def test_renders_screen_reader_announcement_region
+    render_inline(TinComponent.new(form: build_form, method: :text_field, label: "Social Security Number"))
+    assert_selector "[role='alert'][aria-atomic='true'].sr-only"
   end
 end
