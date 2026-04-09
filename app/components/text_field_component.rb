c9f0c03fcc4d2c5221f30668bf3030351733b6b4
DrewProebstelCfa <dproebstel@codeforamerica.org>
2026-04-09 12:12:36 -0700

    remove kwargs

--- app/components/text_field_component.rb
+++ app/components/text_field_component.rb
@@ -1,21 +1,10 @@
 # frozen_string_literal: true
 
 class TextFieldComponent < AttributeBoundFormElementComponent
-  TIN = :tin
-
-  def initialize(form:, method:, label:, help_text: nil, value_type: nil, required: false)
+  def initialize(form:, method:, label:, help_text: nil, required: false)
     super(form: form, method: method)
     @label = label
     @help_text = help_text
-    @value_type = value_type
     @required = required
-  end
-
-  private
-
-  def text_field_kwargs
-    return {} unless @value_type == TIN
-
-    { "x-data" => "{}", "x-mask" => "999-99-9999", "placeholder" => "___-__-____" }
   end
 end
