bde8577aca15be9dc77ef0d82dac52ef2b18544a
Kathryn Hansen <kathrynhansen@Kathryns-MacBook-Pro.local>
2026-04-14 16:11:17 -0500

    add component tests

--- test/components/input_currency_component_test.rb
+++ test/components/input_currency_component_test.rb
@@ -3,10 +3,19 @@
 require "test_helper"
 
 class InputCurrencyComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(InputCurrencyComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  def test_renders_label_and_input
+    render_inline(InputCurrencyComponent.new(form: build_form, method: :text_field, label: "Number"))
+    assert_selector "label", text: "Number"
+    assert_selector "input[type='text']"
+  end
+
+  def test_renders_help_text
+    render_inline(InputCurrencyComponent.new(form: build_form, method: :text_field, label: "Number", help_text: "Enter your Number"))
+    assert_selector ".help_text", text: "Enter your Number"
+  end
+
+  def test_required_adds_class_to_label
+    render_inline(InputCurrencyComponent.new(form: build_form, method: :text_field, label: "Number", required: true))
+    assert_selector "label.required"
   end
 end
