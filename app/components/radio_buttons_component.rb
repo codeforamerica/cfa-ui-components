57c6b8aa715ca2f8e0dcb97dfac09a3e5d51e947
Nick Edwards <nickedwards109@users.noreply.github.com>
2026-04-21 11:54:07 -0700

    Tef 471 distinguish frontend stores with same method (#68)
    * Enable frontend to have more than one of the same form field on the page
    
    This makes it possible for a gubernatorial designation field to exist for
    both the primary and secondary filer on the NJ Gubernatorial Designation page.
    
    * Add test coverage for unique alpine store key
    
    * Update conditional component to accept unique store key, and update LookBook
    
    ---------
    
    Co-authored-by: Nick Edwards <nedwards@codeforamerica.org>

--- app/components/radio_buttons_component.rb
+++ app/components/radio_buttons_component.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class RadioButtonsComponent < AttributeBoundFormElementComponent
-  def initialize(form:, method:, collection:, item_value_method:, item_label_method:, layout: :vertical)
+  def initialize(form:, method:, collection:, item_value_method:, item_label_method:, unique_alpine_store_key: "", layout: :vertical)
     super(form: form, method: method)
     @collection = collection
     @item_value_method = item_value_method
     @item_label_method = item_label_method
+    @unique_alpine_store_key = unique_alpine_store_key
     @layout =
       case layout
       when :horizontal
