b56fc54ca997e61f017ed058bbfdce7431513d86
David Löwenfels <282+dfl@users.noreply.github.com>
2026-04-23 12:48:07 -0700

    Fix routes.rb NameError when gem is consumed as a dependency (#85)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # This is necessary for now since when we try to point at this gem via path or github repo, it brings in the whole
   # project directory structure and therefore tries to draw the routes
-  if Rails.application.instance_of?(CfaUiComponents::Application)
+  if Rails.application.class.name == "CfaUiComponents::Application" # rubocop:disable Style/ClassEqualityComparison
     get "up" => "rails/health#show", :as => :rails_health_check
     mount Lookbook::Engine, at: "/"
   end
