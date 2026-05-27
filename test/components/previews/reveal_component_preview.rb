67e40397fa38c7528cb86f4f0d403705041a0f3d
Kathryn Hansen <kathrynhansen@Kathryns-MacBook-Pro.local>
2026-05-27 15:31:00 -0500

    remove with padding preview

--- test/components/previews/reveal_component_preview.rb
+++ test/components/previews/reveal_component_preview.rb
@@ -10,10 +10,4 @@ class RevealComponentPreview < ViewComponent::Preview
       content_tag(:p, "Sample form card", style: "margin-top: 8px;")
     end
   end
-
-  def with_padding
-    render(RevealComponent.new(summary_text: "I am summary", icon: "question")) do
-      content_tag(:p, "Sample form card", style: "padding-top: 20px;")
-    end
-  end
 end
