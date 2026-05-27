576497e72d3a9e4a13dbc7f70fb8edb29da46286
Kathryn Hansen <kathrynhansen@Kathryns-MacBook-Pro.local>
2026-05-27 16:01:33 -0500

    update select and memorable date to use consistent styling

--- app/components/select_component.rb
+++ app/components/select_component.rb
@@ -10,4 +10,40 @@ class SelectComponent < AttributeBoundFormElementComponent
     @item_label_method = item_label_method
     @optional = optional
   end
+
+  private
+
+  def options
+    @options ||= [
+      {label: "- Select -", value: ""},
+      *@collection.map do |item|
+        {
+          label: item.public_send(@item_label_method).to_s,
+          value: item.public_send(@item_value_method).to_s
+        }
+      end
+    ]
+  end
+
+  def options_json
+    options.to_json
+  end
+
+  def current_value
+    @form.object.public_send(@method).to_s
+  end
+
+  def current_label
+    selected = options.find { |option| option[:value] == current_value }
+    selected&.dig(:label) || "- Select -"
+  end
+
+  def initial_active_index
+    index = options.find_index { |option| option[:value] == current_value }
+    index || 0
+  end
+
+  def required_class
+    @required ? "required" : ""
+  end
 end
