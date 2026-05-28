a1336bb341f96f890bd37d89a53f611c486f40e9
Kathryn Hansen <kathrynhansen@Kathryns-MacBook-Pro.local>
2026-05-28 13:12:52 -0500

    fix spacing in previews

--- test/components/previews/reveal_component_preview.rb
+++ test/components/previews/reveal_component_preview.rb
@@ -1,13 +1,13 @@
 class RevealComponentPreview < ViewComponent::Preview
   def default
     render(RevealComponent.new(summary_text: "I am summary")) do
-      content_tag(:p, "Sample Details", style: "margin-top: 8px;")
+      content_tag(:p, "Sample Details")
     end
   end
 
   def with_icon
     render(RevealComponent.new(summary_text: "I am summary", icon: "question")) do
-      content_tag(:p, "Sample form card", style: "margin-top: 8px;")
+      content_tag(:p, "Sample form card")
     end
   end
 end
