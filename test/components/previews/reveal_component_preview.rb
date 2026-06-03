86f6456076afc6e32ba0fa4a3abd4665fe66c598
Kathryn Hansen <katie112892@gmail.com>
2026-06-03 13:54:39 -0700

    remove with padding preview (#129)
    * remove with padding preview
    
    * fix spacing in previews
    
    ---------
    
    Co-authored-by: Kathryn Hansen <kathrynhansen@Kathryns-MacBook-Pro.local>

--- test/components/previews/reveal_component_preview.rb
+++ test/components/previews/reveal_component_preview.rb
@@ -1,19 +1,13 @@
 class RevealComponentPreview < ViewComponent::Preview
   def default
     render(RevealComponent.new(summary_text: "I am summary")) do
-      content_tag(:p, "Sample Details", style: "margin-top: 8px;")
+      content_tag(:p, "Sample Details")
     end
   end
 
   def with_icon
     render(RevealComponent.new(summary_text: "I am summary", icon: "question")) do
-      content_tag(:p, "Sample form card", style: "margin-top: 8px;")
-    end
-  end
-
-  def with_padding
-    render(RevealComponent.new(summary_text: "I am summary", icon: "question")) do
-      content_tag(:p, "Sample form card", style: "padding-top: 20px;")
+      content_tag(:p, "Sample form card")
     end
   end
 end
