7e3f5bef2133ad474a3a4bd1042c3175b1fad980
David Lowenfels <dlowenfels@codeforamerica.org>
2026-06-04 11:04:42 -0700

    remove m-0

--- test/components/previews/card_component_preview.rb
+++ test/components/previews/card_component_preview.rb
@@ -2,15 +2,15 @@ class CardComponentPreview < FormComponentPreview
   def default
     render(CardComponent.new) do
       content_tag(:p, content_tag(:strong, "Sample form card")) +
-        content_tag(:p, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat ornare elit a vestibulum. Etiam eget sodales ipsum. Etiam accumsan neque erat.", class: "m-0")
+        content_tag(:p, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat ornare elit a vestibulum. Etiam eget sodales ipsum. Etiam accumsan neque erat.")
     end
   end
 
   def with_heading
     render(CardComponent.new) do
       content_tag(:h2, "Sample form card") +
-        content_tag(:p, "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", class: "m-0") +
-        content_tag(:p, "Ut volutpat ornare elit a vestibulum.", class: "m-0")
+        content_tag(:p, "Lorem ipsum dolor sit amet, consectetur adipiscing elit.") +
+        content_tag(:p, "Ut volutpat ornare elit a vestibulum.")
     end
   end
 
@@ -20,12 +20,12 @@ class CardComponentPreview < FormComponentPreview
   def with_interspersed_headings
     render(CardComponent.new) do
       content_tag(:h2, "Name") +
-        content_tag(:p, "Text field 1", class: "m-0") +
-        content_tag(:p, "Text field 2", class: "m-0") +
+        content_tag(:p, "Text field 1") +
+        content_tag(:p, "Text field 2") +
         content_tag(:h2, "Date of birth") +
-        content_tag(:p, "Date picker", class: "m-0") +
+        content_tag(:p, "Date picker") +
         content_tag(:h2, "Social security") +
-        content_tag(:p, "TIN field", class: "m-0")
+        content_tag(:p, "TIN field")
     end
   end
 
@@ -47,8 +47,8 @@ class CardComponentPreview < FormComponentPreview
       card.with_image { content_tag(:div, "", class: "h-[194px] bg-zinc-200") }
       card.with_button { content_tag(:button, "Button", class: "btn btn--primary btn--large") }
       safe_join([
-        content_tag(:h2, "This is the card title", class: "m-0"),
-        content_tag(:p, "This is the card content", class: "m-0")
+        content_tag(:h2, "This is the card title"),
+        content_tag(:p, "This is the card content")
       ])
     end
   end
@@ -57,8 +57,8 @@ class CardComponentPreview < FormComponentPreview
     render(CardComponent.new) do |card|
       card.with_button { content_tag(:button, "Button", class: "btn btn--primary btn--large") }
       safe_join([
-        content_tag(:h2, "This is the card title", class: "m-0"),
-        content_tag(:p, "This is the card content", class: "m-0")
+        content_tag(:h2, "This is the card title"),
+        content_tag(:p, "This is the card content")
       ])
     end
   end
